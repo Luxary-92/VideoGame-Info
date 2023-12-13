e842f19f8cc8806796afe5a70ba93e664a43977e
Luxary <79161171+Luxary-92@users.noreply.github.com>
2023-12-13 08:53:30 +0100

    Call API Example

--- videogame_info/lib/api.dart
+++ videogame_info/lib/api.dart
@@ -1,21 +1,18 @@
 import 'dart:convert';
 
+import 'package:videogame_info/models/deatalles_game.dart';
 import 'package:http/http.dart' as http;
 
-
-
-/*
-Future<List<User>> apiLoadUsers() async {
-  final uri = Uri.parse("https://randomuser.me/api/?results=50");
+Future<List<DetallesGame>> apiLoadUsers() async {
+  final uri = Uri.parse(
+      "https://api.rawg.io/api/games?key=66512237741a4c0e8fbfc7b5d2c21c92");
   final response = await http.get(uri);
   final json = jsonDecode(response.body);
-  final jsonUserList = json["results"];
-  final List<User> userList = [];
-  for (final jsonUser in jsonUserList) {
-    final user = User.fromJson(jsonUser);
-    userList.add(user);
+  final jsonGameDetalles = json["results"];
+  final List<DetallesGame> gameDetalles = [];
+  for (final jsonDetallesGame in jsonGameDetalles) {
+    final detallesGames = DetallesGame.fromJson(jsonDetallesGame);
+    gameDetalles.add(detallesGames);
   }
-  return userList;
+  return gameDetalles;
 }
-
-*/
--- videogame_info/lib/models/deatalles_game.dart
+++ videogame_info/lib/models/deatalles_game.dart
@@ -1,10 +1,15 @@
 class DetallesGame {
   int id;
   String name, description, released;
+  int metacritic;
+  String image, website;
 
   DetallesGame.fromJson(Map<String, dynamic> json)
       : id = json["id"],
         name = json["name"],
         description = json["description"],
-        released = json["released"];
+        released = json["released"],
+        metacritic = json["metacritic"],
+        image = json["image"],
+        website = json["website"];
 }
