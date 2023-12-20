bf397edb77efcf7bdaa2f8aa4524d25e0ee99462
Nixon Correa <ndncorrea@gmail.com>
2023-12-20 08:24:05 +0100

    screens

--- videogame_info/lib/main.dart
+++ videogame_info/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:videogame_info/api.dart';
-import 'package:videogame_info/models/deatalles_game.dart';
-import 'package:videogame_info/widgets/game_details_list.dart';
+import 'package:videogame_info/screens/home_screen.dart';
 //import 'package:flutter/material.dart';
 
 void main() {
@@ -14,56 +12,9 @@ class VideoGameInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          backgroundColor: Color(0xFF3C007A),
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Game'),
-        ),
-        body: DecoratedBox(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF3C007A),
-                Color.fromRGBO(60, 0, 122, 1.0),
-                Color(0xFF681DB7),
-              ],
-              stops: [0.0, 0.35, 1.0],
-            ),
-          ),
-          child: FutureBuilder(
-            future: apiLoadDetailsGames(),
-            builder: (
-              BuildContext context,
-              AsyncSnapshot<List<DetallesGame>> snapshot,
-            ) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              final detailsGamesList = snapshot.data!;
-              return ListView.builder(
-                itemCount: detailsGamesList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return UserListItem(game: detailsGamesList[index]);
-                },
-              );
-            },
-          ),
-        ),
-      ),
+      routes: {
+        "/": (context) => HomeScreen(),
+      },
     );
   }
 }
