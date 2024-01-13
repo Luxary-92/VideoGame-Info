c0e6fd142c7d1190a4a9091a27ca370e9f4359ab
Luxary-92 <79161171+Luxary-92@users.noreply.github.com>
2024-01-13 20:45:34 +0100

    Scroll on info

--- videogame_info/lib/widgets/game_info.dart
+++ videogame_info/lib/widgets/game_info.dart
@@ -13,36 +13,38 @@ class MoreInfoGame extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 450,
-            width: screenSize.width,
-            child:
-                Image(image: NetworkImage(gameInfo.image), fit: BoxFit.cover),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            gameInfo.name,
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 450,
+              width: screenSize.width,
+              child:
+                  Image(image: NetworkImage(gameInfo.image), fit: BoxFit.cover),
             ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: screenSize.width - 60,
-            child: Text(
-              gameInfo.description,
+            const SizedBox(height: 20),
+            Text(
+              gameInfo.name,
               style: const TextStyle(
                 fontSize: 20,
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            SizedBox(
+              width: screenSize.width - 60,
+              child: Text(
+                gameInfo.description,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
